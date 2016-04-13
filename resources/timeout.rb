b56cf1103567f47ed9656d39242a922db011dd48
Bajwa <bik.bajwa@hpe.com>
2016-04-13 11:26:23 -0600

    validate via chef

--- resources/timeout.rb
+++ resources/timeout.rb
@@ -1,12 +1,11 @@
 actions :set
 
 property :ilos, Array, :required => true
-property :timeout, Fixnum
+property :timeout, Fixnum, :equal_to => [15, 30, 60, 120, 0]
 
 include ClientHelper
 
 action :set do
-  raise "Error: Timeout must be 15, 30, 60, 120 or 0 (Infinite)" if not [15, 30, 60, 120, 0].include? timeout
   ilos.each do |ilo|
     client = build_client(ilo)
     cur_val = client.get_timeout
