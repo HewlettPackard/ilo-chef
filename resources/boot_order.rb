9c82d5b22ad6954f8fe7deec1dbee9d0e9a0a7b6
Bajwa <bik.bajwa@hpe.com>
2016-04-15 08:27:15 -0600

    closer

--- resources/boot_order.rb
+++ resources/boot_order.rb
@@ -1,53 +1,28 @@
-actions :revert, :get, :change, :temporary_change
+actions :dump, :set, :set_temporary, :revert
+
 property :ilos, Array, :required => true
 property :boot_order_file, String
-property :new_boot_order, Array
+property :boot_order, Array
 property :boot_target, String
 
-include RestAPI::Helper
-::Chef::Provider.send(:include, ILOINFO)
+include ClientHelper
 
-action :get do
+# TODO: append files using same method as in computer_details
+action :dump do
   ilos.each do |ilo|
-    machine  = ilono.select{|k,v| k == ilo}[ilo]
-    converge_by "Getting boot order and placing in #{boot_order_file}" do
-      get_boot_order(machine, boot_order_file)
-    end
+    client = build_client(ilo)
+    dumpContent = client.get_all
   end
 end
 
-action :change do
-  ilos.each do |ilo|
-    machine  = ilono.select{|k,v| k == ilo}[ilo]
-    old_boot_order = get_boot_order(machine, boot_order_file)[machine["ilo_site"]]
-    if not old_boot_order == new_boot_order
-      converge_by "Setting boot order from #{old_boot_order.to_s} to #{new_boot_order.to_s}" do
-        boot_order_change(machine, new_boot_order)
-        reset_server(machine)
-      end
-    end
-  end
+action :set do
+
 end
 
-action :temporary_change do
-  ilos.each do |ilo|
-    machine  = ilono.select{|k,v| k == ilo}[ilo]
-    old_target = get_temporary_boot_order(machine)
-    if not old_target == boot_target
-      converge_by "Setting temporary boot order from #{old_target} to #{boot_target}" do
-        boot_order_change_temporary(machine, boot_target)
-      end
-    end
-  end
+action :set_temporary do
+
 end
 
 action :revert do
-  ilos.each do |ilo|
-    machine  = ilono.select{|k,v| k == ilo}[ilo]
-    if not get_boot_order_baseconfig(machine) == "default"
-      converge_by "Reverting boot order to default" do
-        boot_order_revert(machine)
-      end
-    end
-  end
+
 end
