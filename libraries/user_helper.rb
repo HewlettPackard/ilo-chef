6a92fdef6c63002e42498fcea46bb2ff816a8896
Bajwa <bik.bajwa@hpe.com>
2016-04-25 14:03:25 -0600

    refactored and tested

--- libraries/user_helper.rb
+++ libraries/user_helper.rb
@@ -1,49 +1,6 @@
 module ILO_SDK
   # Contains helper methods for user actions
   module User_Helper
-    # Get the UID indicator LED state
-    # @raise [RuntimeError] if the request failed
-    # @return [String] state
-    #def get_indicator_led
-    #  response = rest_get('/redfish/v1/Systems/1/')
-    #  response_handler(response)['IndicatorLED']
-    #end
-
-    # Set the UID indicator LED
-    # @param [String, Symbol] State
-    # @raise [RuntimeError] if the request failed
-    # @return true
-    #def set_indicator_led(state)
-    #  newAction = { 'IndicatorLED' => state }
-    #  response = rest_patch('/redfish/v1/Systems/1/', body: newAction)
-    #  response_handler(response)
-    #  true
-    #end
-    #def reset_user_password(machine, modUserName, modPassword)
-    #  uad = rest_api(:get, '/redfish/v1/AccountService/Accounts/', machine)
-    #  userhref = adminhref(uad, modUserName)
-    #  options = {
-    #    'body' => modPassword
-    #  }
-    #  rest_api(:patch, userhref, machine, options)
-    #end
-
-    #def delete_user(deleteUserName, machine)
-    #  uad = rest_api(:get, '/redfish/v1/AccountService/Accounts/', machine)
-    #  minhref = adminhref(uad, deleteUserName )
-    #  rest_api(:delete, minhref, machine)
-    #end
-
-    #def get_users(machine)
-    #  rest_api(:get, '/rest/v1/AccountService/Accounts', machine)["Items"].collect{|user| user["UserName"]}
-    #end
-
-    #def create_user(machine,username,password)
-    #  rest_api(:get, '/rest/v1/AccountService/Accounts', machine)
-    #  newUser = {"UserName" => username, "Password"=> password, "Oem" => {"Hp" => {"LoginName" => username} }}
-    #  options = {'body' => newUser}
-    #  rest_api(:post, '/redfish/v1/AccountService/Accounts/', machine,  options)
-    #end
     def userhref(uri, username)
       response = rest_get(uri)
       items = response_handler(response)['Items']
@@ -54,25 +11,48 @@ module ILO_SDK
       end
     end
 
+    # Get the users
+    # @raise [RuntimeError] if the request failed
+    # @return [String] users
     def get_users
       response = rest_get('/redfish/v1/AccountService/Accounts/')
-      response_handler(response)['Items'].collect{|user| user['Username']}
+      items = response_handler(response)['Items'].collect{|user| user['UserName']}
     end
 
+    # Create a user
+    # @param [String, Symbol] username
+    # @param [String, Symbol] password
+    # @raise [RuntimeError] if the request failed
+    # @return true
     def create_user(username, password)
       newAction = {"UserName" => username, "Password"=> password, "Oem" => {"Hp" => {"LoginName" => username}}}
       response = rest_post('/redfish/v1/AccountService/Accounts/', body: newAction)
+      response_handler(response)
       true
     end
 
-    def reset_user_password(username, password)
+    # Change the password for a user
+    # @param [String, Symbol] username
+    # @param [String, Symbol] password
+    # @raise [RuntimeError] if the request failed
+    # @return true
+    def change_password(username, password)
+      newAction = {"Password" => password}
       userhref = userhref('/redfish/v1/AccountService/Accounts/', username)
-      newAction = {}
       response = rest_patch(userhref, body: newAction)
+      response_handler(response)
       true
     end
 
+    # Delete a specific user
+    # @param [String, Symbol] username
+    # @raise [RuntimeError] if the request failed
+    # @return true
     def delete_user(username)
+      userhref = userhref('/redfish/v1/AccountService/Accounts/', username)
+      response = rest_delete(userhref)
+      response_handler(response)
+      true
     end
   end
 end
--- resources/user.rb
+++ resources/user.rb
@@ -1,65 +1,40 @@
-actions :createUser, :deleteUser, :changePassword
-default_action :createUser
+actions :create, :delete, :changePassword
 
+property :ilos, Array, :required => true
 property :username, String
 property :password, String
-property :ilo_names, [Array,Symbol]
-include RestAPI::Helper
-include ::ILOINFO
-action :createUser do
-	if ilo_names.class == Array
-		ilo_names.each do |ilo|
-			machine  = ilono[ilo]
-			fail "ilo #{ilo} not defined in configuration!" unless machine
-			next if get_users(machine).include?(username)
-			converge_by "Creating user #{username} for #{ilo} - " do
-				create_user(machine,username,password)
-			end
-		end
-	else
-		ilono.each do |name,site|
-			next if get_users(site).include?(username)
-			converge_by "Creating user #{username} for #{site} - " do
-				create_user(site, username, password)
-			end
+
+include ClientHelper
+
+action :create do
+	ilos.each do |ilo|
+		client = build_client(ilo)
+		cur_users = client.get_users
+		next if cur_users.include? username
+		converge_by "Create user #{username} on ilo #{client.host}" do
+			client.create_user(username, password)
 		end
 	end
 end
 
-action :deleteUser do
-	if ilo_names.class == Array
-		ilo_names.each do |ilo|
-			machine = ilono[ilo]
-			fail "ilo #{ilo} not defined in configuration!" unless machine
-			next if !get_users(machine).include?(username)
-			converge_by "Deleting user #{username} from #{ilo} - " do
-				delete_user(username,machine)
-			end
-		end
-	else
-		ilono.each do |name,site|
-			next if get_users(site).include?(username)
-			converge_by "Deleting user #{username} from #{site} - " do
-				delete_user(username,site)
-			end
+action :delete do
+	ilos.each do |ilo|
+		client = build_client(ilo)
+		cur_users = client.get_users
+		next if not cur_users.include? username
+		converge_by "Delete user #{username} on ilo #{client.host}'" do
+			client.delete_user(username)
 		end
 	end
 end
 
 action :changePassword do
-	if ilo_names.class == Array
-		ilo_names.each do |ilo|
-			machine = ilono[ilo]
-			fail "ilo #{ilo} not defined in configuration!" unless machine
-			converge_by "Changing password for #{username} in #{ilo}- " do
-			reset_user_password(machine,username,password)
+	ilos.each do |ilo|
+		client = build_client(ilo)
+		cur_users = client.get_users
+		next if not cur_users.include? username
+		converge_by "Change password for user #{username} on ilo #{client.host}" do
+			client.change_password(username, password)
 		end
-		end
-	else
-		ilono.each do |name,site|
-			converge_by "Changing password for #{username} in #{site}- " do
-			reset_user_password(site, username, password)
-		end
-	  end
 	end
 end
