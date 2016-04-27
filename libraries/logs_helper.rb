67ff5c42d2bb3e13ccf6e9349d88d21e260751a2
Bajwa <bik.bajwa@hpe.com>
2016-04-27 13:49:49 -0600

    refactored the log resource

--- libraries/logs_helper.rb
+++ libraries/logs_helper.rb
@@ -1,69 +1,10 @@
 module ILO_SDK
   # Contains helper methods for Logs actions
   module Logs_Helper
-    # Get the UID indicator LED state
-    # @raise [RuntimeError] if the request failed
-    # @return [String] state
-    #def get_indicator_led
-    #  response = rest_get('/redfish/v1/Systems/1/')
-    #  response_handler(response)['IndicatorLED']
-    #end
-
-    # Set the UID indicator LED
-    # @param [String, Symbol] State
+    # Clear the specified logs
+    # @param [String, Symbol] log_type
     # @raise [RuntimeError] if the request failed
     # @return true
-    #def set_indicator_led(state)
-    #  newAction = { 'IndicatorLED' => state }
-    #  response = rest_patch('/redfish/v1/Systems/1/', body: newAction)
-    #  response_handler(response)
-    #  true
-    #end
-
-    #def clear_iel_logs(machine)
-    #  newAction = {"Action"=> "ClearLog"}
-    #  options = {'body' => newAction}
-    #  rest_api(:post, '/redfish/v1/Managers/1/LogServices/IEL/', machine, options)
-    #end
-
-    #def clear_iml_logs(machine)
-    #  newAction = {"Action"=> "ClearLog"}
-    #  options = {'body' => newAction}
-    #  rest_api(:post, '/redfish/v1/Systems/1/LogServices/IML/', machine, options)
-    #end
-
-    #def dump_iel_logs(machine,ilo,severity_level,file,duration)
-    #  entries = rest_api(:get, '/redfish/v1/Managers/1/LogServices/IEL/Entries/', machine)["links"]["Member"]
-    #  entries.each do |e|
-    #    logs = rest_api(:get, e["href"], machine)
-    #    severity = logs["Severity"]
-    #    message = logs["Message"]
-    #    created = logs["Created"]
-    #    if !severity_level.nil?
-    #      ilo_log_entry = "#{ilo} | #{severity} | #{message} | #{created} \n" if created == severity_level and Time.parse(created) > (Time.now.utc - (duration*3600))
-    #    else
-    #      ilo_log_entry = "#{ilo} | #{severity} | #{message} | #{created} \n" if Time.parse(created) > (Time.now.utc - (duration*3600))
-    #    end
-    #    File.open("#{Chef::Config[:file_cache_path]}/#{file}.txt", 'a+') {|f| f.write(ilo_log_entry) }
-    #  end
-    #end
-
-    #def dump_iml_logs(machine, ilo, severity_level,file,duration)
-    #  entries = rest_api(:get, '/redfish/v1/Systems/1/LogServices/IML/Entries/', machine)["links"]["Member"]
-    #  entries.each do |e|
-    #    logs = rest_api(:get, e["href"], machine)
-    #    severity = logs["Severity"]
-    #    message = logs["Message"]
-    #    created = logs["Created"]
-    #    if !severity_level.nil?
-    #      ilo_log_entry = "#{ilo} | #{severity} | #{message} | #{created} \n" if created == severity_level and Time.parse(created) > (Time.now.utc - (duration*3600))
-    #    else
-    #      ilo_log_entry = "#{ilo} | #{severity} | #{message} | #{created} \n" if Time.parse(created) > (Time.now.utc - (duration*3600))
-    #    end
-    #    File.open("#{Chef::Config[:file_cache_path]}/#{file}.txt", 'a+') {|f| f.write(ilo_log_entry) }
-    #  end
-    #end
-
     def clear_logs(log_type)
       newAction = {"Action" => "ClearLog"}
       response = rest_post("/redfish/v1/Managers/1/LogServices/#{log_type}/", body: newAction)
@@ -71,49 +12,33 @@ module ILO_SDK
       true
     end
 
-    def logs_empty?(dump_file)
+    # Check to see if the logs are empty
+    # @param [String, Symbol] log_type
+    # @raise [RuntimeError] if the request failed
+    # @return true or false
+    def logs_empty?(log_type)
+      response = rest_get("/redfish/v1/Managers/1/LogServices/#{log_type}/Entries/")
+      response_handler(response)['Items'].empty?
     end
 
+    # Get the specified logs
+    # @param [String, Symbol] severity_level
+    # @param [String, Symbol] duration
+    # @param [String, Symbol] log_type
+    # @raise [RuntimeError] if the request failed
+    # @return log_entries
     def get_logs(severity_level, duration, log_type)
       response = rest_get("/redfish/v1/Managers/1/LogServices/#{log_type}/Entries/")
-      entries = response_handler(response)['links']['Member']
+      entries = response_handler(response)['Items']
       log_entries = []
       entries.each do |e|
         if !severity_level.nil?
-          log_entries.insert("#{ilo} | #{severity} | #{message} | #{created} \n" if created == severity_level and Time.parse(created) > (Time.now.utc - (duration*3600)))
+          log_entries.push("#{e['Severity']} | #{e['Message']} | #{e['Created']}") if e['Severity'] == severity_level and Time.parse(e['Created']) > (Time.now.utc - (duration*3600))
         else
-          log_entries.insert("#{ilo} | #{severity} | #{message} | #{created} \n" if Time.parse(created) > (Time.now.utc - (duration*3600)))
+          log_entries.push("#{e['Severity']} | #{e['Message']} | #{e['Created']}") if Time.parse(e['Created']) > (Time.now.utc - (duration*3600))
         end
-        return log_entries
       end
-    end
-
-    def get_iel_logs(severity_level, duration)
-      response = rest_get('/redfish/v1/Managers/1/LogServices/IEL/Entries/')
-      entries = response_handler(response)['links']['Member']
-      log_entries = []
-      entries.each do |e|
-        if !severity_level.nil?
-          log_entries.insert("#{ilo} | #{severity} | #{message} | #{created} \n" if created == severity_level and Time.parse(created) > (Time.now.utc - (duration*3600)))
-        else
-          log_entries.insert("#{ilo} | #{severity} | #{message} | #{created} \n" if Time.parse(created) > (Time.now.utc - (duration*3600)))
-        end
-        return log_entries
-      end
-    end
-
-    def get_iml_logs(severity_level, duration)
-      response = rest_get('/redfish/v1/Managers/1/LogServices/IEL/Entries/')
-      entries = response_handler(response)['links']['Member']
-      log_entries = []
-      entries.each do |e|
-        if !severity_level.nil?
-          log_entries.insert("#{ilo} | #{severity} | #{message} | #{created} \n" if created == severity_level and Time.parse(created) > (Time.now.utc - (duration*3600)))
-        else
-          log_entries.insert("#{ilo} | #{severity} | #{message} | #{created} \n" if Time.parse(created) > (Time.now.utc - (duration*3600)))
-        end
-        return log_entries
-      end
+      return log_entries
     end
   end
 end
--- resources/logs.rb
+++ resources/logs.rb
@@ -1,23 +1,35 @@
 actions :clear, :dump
 
-pproperty :ilos, Array, :required => true
+property :ilos, Array, :required => true
 property :log_type, String, :required => true, :equal_to => ["IEL", "IML"]
 property :severity_level, String, :equal_to => ["OK","Warning","Critical"]
-property :dump_file, String, :required => true
-property :duration_in_hours, Integer, :required => true, :default => 24
+property :dump_file, String
+property :owner, [String, Integer], default: node['current_user']
+property :group, [String, Integer], default: node['current_user']
+property :duration, Integer, :default => 24
 
 include ClientHelper
 
 action :clear do
   ilos.each do |ilo|
     client = build_client(ilo)
-    next if logs_empty?(dump_file)
+    next if client.logs_empty?(log_type)
     converge_by "Clear ilo #{client.host} #{log_type} logs" do
-      clear_logs(log_type)
+      client.clear_logs(log_type)
     end
   end
 end
 
 action :dump do
-
+  dumpContent = {}
+  ilos.each do |ilo|
+    client = build_client(ilo)
+    host = ilo[:host] || ilo['host']
+    dumpContent[host.to_s] = client.get_logs(severity_level, duration, log_type).to_yaml
+  end
+  file dump_file do
+    owner owner
+    group group
+    content dumpContent.to_yaml
+  end
 end
