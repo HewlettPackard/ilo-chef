e2fca36e52ef1b823d669672fcd7ba961a33eaef
Bajwa <bik.bajwa@hpe.com>
2016-05-03 16:29:30 -0600

    insert and eject are tested and working

--- libraries/virtual_media_helper.rb
+++ libraries/virtual_media_helper.rb
@@ -13,34 +13,24 @@ module ILO_SDK
         media[virtual_media['Id']] =
         {
           'Image' => virtual_media['Image'],
-          'MediaTypes' => virtual_media['MediaTypes'],
-          'BootOnNextServerReset' => virtual_media['Oem']['Hp']['BootOnNextServerReset']
+          'MediaTypes' => virtual_media['MediaTypes']
         }
       end
       return media
     end
 
-    # Set the Virtual Media Mount
-    # @param [String, Symbol] id
-    # @param [String, Symbol] iso_uri
-    # @param [String, Symbol] boot_on_next_server_reset
+    # Return whether Virtual Media is inserted
     # @raise [RuntimeError] if the request failed
-    # @return true
-    def set_virtual_media(id, iso_uri, boot_on_next_server_reset)
-      newAction = {
-        'Image' => iso_uri,
-        'Oem' => {'Hp' => {'BootOnNextServerReset' => boot_on_next_server_reset}}
-      }
-      response = rest_patch("/redfish/v1/Managers/1/VirtualMedia/#{id}/", body: newAction)
-      response_handler(response)
-      true
-    end
-
+    # @return [TrueClass, FalseClass] inserted
     def is_virtual_media_inserted?(id)
       response = rest_get('/redfish/v1/Managers/1/VirtualMedia/2/')
       response_handler(response)['Inserted']
     end
 
+    # Insert Virtual Media
+    # @param [String, Symbol] id
+    # @param [String, Symbol] image
+    # @return true
     def insert_virtual_media(id, image)
       newAction = {
         "Action" => "InsertVirtualMedia",
@@ -52,6 +42,9 @@ module ILO_SDK
       true
     end
 
+    # Eject Virtual Media
+    # @param [String, Symbol] id
+    # @return true
     def eject_virtual_media(id)
       newAction = {
         "Action" => "EjectVirtualMedia",
--- resources/virtual_media.rb
+++ resources/virtual_media.rb
@@ -2,24 +2,8 @@ actions :mount, :insert, :eject
 
 property :ilos, Array, :required => true
 property :iso_uri, String, :required => true, :regex => /^$|^(ht|f)tp:\/\/[A-Za-z0-9]([-.\w:]*[A-Za-z0-9])([A-Za-z0-9\-\.\?,'\/\\\+&;%\$#~=_]*)?(.iso)$/
-property :boot_on_next_server_reset, [TrueClass,FalseClass], :default => false
 
 include ClientHelper
-
-action :mount do
-  ilos.each do |ilo|
-    client = build_client(ilo)
-    cur_val = client.get_virtual_media
-    cur_val.each do |key, hash|
-      if hash['MediaTypes'].include?('CD') || hash['MediaTypes'].include?('DVD')
-        next if hash['Image'] == iso_uri && hash['BootOnNextServerReset'] == boot_on_next_server_reset
-        converge_by "Set ilo #{client.host} ISO URI to '#{iso_uri}' and boot on next server rest to '#{boot_on_next_server_reset.to_s}'" do
-          client.set_virtual_media(key, iso_uri, boot_on_next_server_reset)
-        end
-      end
-    end
-  end
-end
 
 action :insert do
   ilos.each do |ilo|
