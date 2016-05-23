daf3810972a63fb8b27b7f49ae58255ff981c01f
Bajwa <bik.bajwa@hpe.com>
2016-05-23 09:52:50 -0600

    cleaned the date_time class

--- resources/date_time.rb
+++ resources/date_time.rb
@@ -1,9 +1,9 @@
-actions :set, :use_ntp, :set_ntp_servers
+actions :set
 
 property :ilos, Array, :required => true
 property :time_zone, String
 property :ntp_servers, Array
-property :value, [TrueClass, FalseClass], :required => true
+property :use_ntp, [TrueClass, FalseClass], :required => true
 
 action_class do
   include IloHelper
@@ -13,34 +13,26 @@ action :set do
   load_sdk
   ilos.each do |ilo|
     client = build_client(ilo)
-    cur_val = client.get_time_zone
-    next if cur_val == time_zone
-    converge_by "Set ilo #{client.host} time zone from '#{cur_val.to_s}' to '#{time_zone.to_s}'" do
-      client.set_time_zone(time_zone)
+    if time_zone
+      cur_val = client.get_time_zone
+      next if cur_val == time_zone
+      converge_by "Set ilo #{client.host} time zone from '#{cur_val.to_s}' to '#{time_zone.to_s}'" do
+        client.set_time_zone(time_zone)
+      end
     end
-  end
-end
-
-action :set_ntp do
-  load_sdk
-  ilos.each do |ilo|
-    client = build_client(ilo)
-    cur_val = client.get_ntp
-    next if cur_val == value
-    converge_by "Set ilo #{client.host} NTP use to '#{value.to_s}'" do
-      client.set_ntp(value)
+    if ntp_servers
+      cur_val = client.get_ntp
+      next if cur_val == use_ntp
+      converge_by "Set ilo #{client.host} NTP use from '#{cur_val.to_s}' to '#{use_ntp.to_s}'" do
+        client.set_ntp(use_ntp)
+      end
     end
-  end
-end
-
-action :set_ntp_servers do
-  load_sdk
-  ilos.each do |ilo|
-    client = build_client(ilo)
-    cur_val = client.get_ntp_servers
-    next if cur_val == ntp_servers
-    converge_by "Set ilo #{client.host} NTP Servers from '#{cur_val.to_s}' to '#{ntp_servers.to_s}'" do
-      client.set_ntp_servers(ntp_servers)
+    if use_ntp
+      cur_val = client.get_ntp_servers
+      next if cur_val == ntp_servers
+      converge_by "Set ilo #{client.host} NTP Servers from '#{cur_val.to_s}' to '#{ntp_servers.to_s}'" do
+        client.set_ntp_servers(ntp_servers)
+      end
     end
   end
 end
