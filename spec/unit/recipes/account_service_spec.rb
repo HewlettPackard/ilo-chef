788e01304e23fb348da47012192373d73a2b4ce7
Bajwa <bik.bajwa@hpe.com>
2016-06-17 10:38:56 -0600

    testing the opposites for account_service

--- spec/unit/recipes/account_service_spec.rb
+++ spec/unit/recipes/account_service_spec.rb
@@ -15,6 +15,11 @@ describe 'ilo_test::account_service_create' do
     expect_any_instance_of(ILO_SDK::Client).to receive(:create_user).with('test', 'test123').and_return(true)
     expect(real_chef_run).to create_ilo_account_service('create test account')
   end
+
+  it 'does not create test account' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_users).and_return(['user1', 'user2', 'test'])
+    expect(real_chef_run).to create_ilo_account_service('create test account')
+  end
 end
 
 describe 'ilo_test::account_service_changePassword' do
@@ -23,6 +28,12 @@ describe 'ilo_test::account_service_changePassword' do
 
   it 'change test account password' do
     expect_any_instance_of(ILO_SDK::Client).to receive(:get_users).and_return(['user1', 'user2', 'test'])
+    expect_any_instance_of(ILO_SDK::Client).to receive(:change_password).with('test', 'newtest123').and_return(true)
+    expect(real_chef_run).to changePassword_ilo_account_service('change test account password')
+  end
+
+  it 'does not change test account password' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_users).and_return(['user1', 'user2'])
     expect_any_instance_of(ILO_SDK::Client).to receive(:change_password).with('test', 'newtest123').and_return(true)
     expect(real_chef_run).to changePassword_ilo_account_service('change test account password')
   end
@@ -35,6 +46,11 @@ describe 'ilo_test::account_service_delete' do
   it 'delete test account' do
     expect_any_instance_of(ILO_SDK::Client).to receive(:get_users).and_return(['user1', 'user2', 'test'])
     expect_any_instance_of(ILO_SDK::Client).to receive(:delete_user).with('test').and_return(true)
+    expect(real_chef_run).to delete_ilo_account_service('delete test account')
+  end
+
+  it 'does not delete test account' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_users).and_return(['user1', 'user2'])
     expect(real_chef_run).to delete_ilo_account_service('delete test account')
   end
 end
