8038c6df4b8005e116de71fad97d8bf8e8622d6d
Bajwa <bik.bajwa@hpe.com>
2016-06-17 10:46:47 -0600

    made ilos generic

--- test/fixtures/cookbooks/ilo_test/recipes/account_service_changePassword.rb
+++ test/fixtures/cookbooks/ilo_test/recipes/account_service_changePassword.rb
@@ -1,9 +1,8 @@
 ilo =
   {
-    host: '16.85.179.199',
+    host: 'ilo.example.com',
     user: 'Admin',
-    password: 'admin123',
-    ssl_enabled: false
+    password: 'secret123'
   }
 
 ilo_account_service 'change test account password' do
--- test/fixtures/cookbooks/ilo_test/recipes/account_service_delete.rb
+++ test/fixtures/cookbooks/ilo_test/recipes/account_service_delete.rb
@@ -1,11 +1,10 @@
 ilo =
   {
-    host: '16.85.179.199',
+    host: 'ilo.example.com',
     user: 'Admin',
-    password: 'admin123',
-    ssl_enabled: false
+    password: 'secret123'
   }
-  
+
 ilo_account_service 'delete test account' do
   ilos [ilo]
   username 'test'
