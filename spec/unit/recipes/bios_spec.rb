e0d88c3ca475b709766d34f23b21b648ccc89df8
Bajwa <bik.bajwa@hpe.com>
2016-06-17 15:29:50 -0600

    added spec for boot_settings

--- spec/unit/recipes/bios_spec.rb
+++ spec/unit/recipes/bios_spec.rb
@@ -9,6 +9,11 @@ describe 'ilo_test::bios_revert' do
     expect_any_instance_of(ILO_SDK::Client).to receive(:revert_bios).and_return(true)
     expect(real_chef_run).to revert_ilo_bios('revert bios')
   end
+
+  it 'does not revert bios' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_bios_baseconfig).and_return('default')
+    expect(real_chef_run).to revert_ilo_bios('revert bios')
+  end
 end
 
 describe 'ilo_test::bios_set' do
@@ -37,7 +42,29 @@ describe 'ilo_test::bios_set' do
     expect_any_instance_of(ILO_SDK::Client).to receive(:set_uefi_shell_startup).with('Enabled', 'NetworkLocation', 'http://www.test.nsh').and_return(true)
     expect_any_instance_of(ILO_SDK::Client).to receive(:set_bios_dhcp).with('Enabled').and_return(true)
     expect_any_instance_of(ILO_SDK::Client).to receive(:set_url_boot_file).with('http://www.urlbootfiletest.iso').and_return(true)
-    expect_any_instance_of(ILO_SDK::Client).to receive(:set_bios_service).with('bik', 'bik.bajwa@hpe.com')
+    expect_any_instance_of(ILO_SDK::Client).to receive(:set_bios_service).with('bik', 'bik.bajwa@hpe.com').and_return(true)
+    expect(real_chef_run).to set_ilo_bios('set bios')
+  end
+
+  it 'does not set bios' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_uefi_shell_startup).and_return({
+      'UefiShellStartup' => 'Enabled',
+      'UefiShellStartupLocation' => 'NetworkLocation',
+      'UefiShellStartupUrl' => 'http://www.test.nsh'
+    })
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_bios_dhcp).and_return({
+      'Dhcpv4' => 'Enabled',
+      'Ipv4Address' => '111.111.111.111',
+      'Ipv4Gateway' => '111.111.111.111',
+      'Ipv4PrimaryDNS' => '111.111.111.111',
+      'Ipv4SecondaryDNS' => '111.111.111.111',
+      'Ipv4SubnetMask' => '111.111.111.111'
+    })
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_url_boot_file).and_return('http://www.urlbootfiletest.iso')
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_bios_service).and_return({
+      'ServiceName' => 'bik',
+      'ServiceEmail' => 'bik.bajwa@hpe.com'
+    })
     expect(real_chef_run).to set_ilo_bios('set bios')
   end
 end
---
+++ spec/unit/recipes/boot_settings_spec.rb
@@ -0,0 +1,74 @@
+require_relative './../../spec_helper'
+
+describe 'ilo_test::boot_settings_revert' do
+  let(:resource_name) { 'boot_settings' }
+  include_context 'chef context'
+
+  it 'revert boot settings' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_boot_baseconfig).and_return('not_default')
+    expect_any_instance_of(ILO_SDK::Client).to receive(:revert_boot).and_return(true)
+    expect(real_chef_run).to revert_ilo_boot_settings('revert boot settings')
+  end
+
+  it 'does not revert boot settings' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_boot_baseconfig).and_return('default')
+    expect(real_chef_run).to revert_ilo_boot_settings('revert boot settings')
+  end
+end
+
+describe 'ilo_test::boot_settings_set' do
+  let(:resource_name) { 'boot_settings' }
+  include_context 'chef context'
+
+  it 'set boot settings' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_boot_order).and_return([
+      'Generic.USB.1.1',
+      'FD.Virtual.1.1',
+      'HD.Emb.1.1',
+      'HD.Emb.1.2',
+      'NIC.LOM.1.1.IPv4',
+      'NIC.LOM.1.1.IPv6'
+    ])
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_temporary_boot_order).and_return('Auto')
+    expect_any_instance_of(ILO_SDK::Client).to receive(:set_boot_order).with([
+      'FD.Virtual.1.1',
+      'Generic.USB.1.1',
+      'HD.Emb.1.1',
+      'HD.Emb.1.2',
+      'NIC.LOM.1.1.IPv4',
+      'NIC.LOM.1.1.IPv6'
+    ]).and_return(true)
+    expect_any_instance_of(ILO_SDK::Client).to receive(:set_temporary_boot_order).with('None').and_return(true)
+    expect(real_chef_run).to set_ilo_boot_settings('set boot settings')
+  end
+
+  it 'does not set boot settings' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_boot_order).and_return([
+      'FD.Virtual.1.1',
+      'Generic.USB.1.1',
+      'HD.Emb.1.1',
+      'HD.Emb.1.2',
+      'NIC.LOM.1.1.IPv4',
+      'NIC.LOM.1.1.IPv6'
+    ])
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_temporary_boot_order).and_return('None')
+    expect(real_chef_run).to set_ilo_boot_settings('set boot settings')
+  end
+end
+
+describe 'ilo_test::boot_settings_dump' do
+  let(:resource_name) { 'boot_settings' }
+  include_context 'chef context'
+
+  it 'dump boot settings' do
+    expect_any_instance_of(ILO_SDK::Client).to receive(:get_boot_order).and_return([
+      'FD.Virtual.1.1',
+      'Generic.USB.1.1',
+      'HD.Emb.1.1',
+      'HD.Emb.1.2',
+      'NIC.LOM.1.1.IPv4',
+      'NIC.LOM.1.1.IPv6'
+    ])
+    expect(real_chef_run).to dump_ilo_boot_settings('dump boot settings')
+  end
+end
