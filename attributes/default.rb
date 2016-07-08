8b46206acab30a5768a2dd193d0a9bd02979d27e
Bajwa <bik.bajwa@hpe.com>
2016-07-08 10:23:28 -0600

    bumped version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,4 @@
 # Attributes:: default
 #
 
-# default['ilo']['ruby_sdk_version'] = '~> 1.0'
-default['ilo']['ruby_sdk_version'] = '>= 0.1' # TODO: Replace with above line once the sdk is released
+default['ilo']['ruby_sdk_version'] = '~> 1.0'
