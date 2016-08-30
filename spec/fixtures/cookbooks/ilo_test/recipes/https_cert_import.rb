57963db79cfa76a37649419b2bd117e357b1a80e
Jared Smartt <jared.smartt@hpe.com>
2016-08-30 12:29:59 -0600

    test recipes use ilos attribute

--- spec/fixtures/cookbooks/ilo_test/recipes/https_cert_import.rb
+++ spec/fixtures/cookbooks/ilo_test/recipes/https_cert_import.rb
@@ -1,12 +1,20 @@
-ilo1 =
-  {
-    host: 'ilo.example.com',
-    user: 'Admin',
-    password: 'secret123'
-  }
+# Cookbook Name:: ilo_test
+# Recipe:: https_cert_import
+#
+# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
 
 ilo_https_cert 'import certificate' do
-  ilo ilo1
+  ilo node['ilo_test']['ilo1']
   certificate 'example_certificate'
   file_path '/c/example_file'
   action :import
